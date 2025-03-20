9b5772060ac58d2cf2961cb4a5534a8ddd4bc193
ARJ2211 <aayushrj22@gmail.com>
2025-03-19 22:42:13 -0400

    API template added

--- platform_app/platform_app_core/urls.py
+++ platform_app/platform_app_core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import users.views as UserViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('save_user/', UserViews.save_user)
 ]
--- platform_app/users/views.py
+++ platform_app/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from users.models import User
 # Create your views here.
+
+
+@csrf_exempt
+def save_user(request):
+    import pdb; pdb.set_trace()
+    data = {
+        'message': 'Success',
+        'status': 200,
+        'data': {}
+    }
+    return JsonResponse(data)
