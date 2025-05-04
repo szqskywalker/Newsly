804b667ba0f0cacec05e9429b80645034643e043
Zameer Qasim <syedzameer212121@gmail.com>
2025-05-04 00:49:05 -0400

    .

--- platform_app/platform_app_core/urls.py
+++ platform_app/platform_app_core/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('save_user/', UserViews.save_user),
     path('get_user/', UserViews.get_user),
     path('get_sources/', UserViews.get_sources),
-    path('search_news/', UserViews.search_news)
+    path('search_news/', UserViews.search_news),
     path('health/', UserViews.health)
 ]
