448f8dcab12c4c79cd4c8689dd880352542e817d
Zameer Qasim <syedzameer212121@gmail.com>
2025-05-04 01:14:54 -0400

    .

--- platform_app/platform_app_core/settings.py
+++ platform_app/platform_app_core/settings.py
@@ -35,10 +35,12 @@ ALLOWED_HOSTS = [
     "419a-155-246-151-34.ngrok-free.app",
     "2c13-2600-4041-44a7-d900-789f-bdae-f2bd-d51a.ngrok-free.app",
     "a227-2600-4041-44a7-d900-789f-bdae-f2bd-d51a.ngrok-free.app",
-    "localhost"
+    "localhost",
+    "https://newsly-backend-2l4l.onrender.com",
+    "newsly-backend-2l4l.onrender.com"
 ]
 
-ALLOWED_HOSTS.extend(os.getenv("DJANGO_ALLOWED_HOSTS"))
+# ALLOWED_HOSTS.extend(os.getenv("DJANGO_ALLOWED_HOSTS"))
 # Application definition
 
 INSTALLED_APPS = [
