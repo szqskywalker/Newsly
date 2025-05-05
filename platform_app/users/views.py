ba221893cfa7054d3212a1f0159f5d9ea0e7c0ad
ARJ2211 <aayushrj22@gmail.com>
2025-05-05 15:56:38 -0400

    migrated sources to UI

--- platform_app/users/views.py
+++ platform_app/users/views.py
@@ -89,11 +89,11 @@ def get_sources(request):
 @csrf_exempt
 def search_news(request):
     headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "application/json,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive"
+        "X-Api-Key": "3d83b1afc782411490c8c8ebde73f320",
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        )
     }
     api_key = "3d83b1afc782411490c8c8ebde73f320"
     base_url = "https://newsapi.org/v2/everything"
